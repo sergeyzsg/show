a7492ecac123e126493cdf21c0b3ea359fe3555b
Sergey Chuprakov <sergeyzsg@yandex.ru>
2014-08-18 14:11:35 +0600

    Fixed default mezzanine template

--- local_settings.template.py
+++ local_settings.template.py
@@ -1,8 +1,8 @@
 DEBUG = True
 
 # Make these unique, and don't share it with anybody.
-SECRET_KEY = "282c20c4-521b-4e16-82b5-cc8f3d24242f7550472f-606c-45b2-a2d2-c54f041cb600398ea041-7386-46c4-805f-955e94291aff"
-NEVERCACHE_KEY = "b90915ba-489c-4829-8571-d2ebc5c6889761770c3e-5338-4458-8571-df1bb9b148561c8758d7-f151-4a13-b5b3-17793bf292c2"
+SECRET_KEY = ""
+NEVERCACHE_KEY = ""
 
 DATABASES = {
     "default": {
@@ -20,3 +20,5 @@ DATABASES = {
         "PORT": "",
     }
 }
+
+TIME_ZONE = 'Europe/Moscow'
--- settings.py
+++ settings.py
@@ -198,7 +198,7 @@ SITE_ID = 1
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
-USE_I18N = False
+USE_I18N = True
 
 # Tuple of IP addresses, as strings, that:
 #   * See debug comments, when DEBUG is true
@@ -307,6 +307,9 @@ INSTALLED_APPS = (
     "django.contrib.sites",
     "django.contrib.sitemaps",
     "django.contrib.staticfiles",
+
+    'mycart',
+
     "cartridge.shop",
     "mezzanine.boot",
     "mezzanine.conf",
@@ -316,7 +319,8 @@ INSTALLED_APPS = (
     "mezzanine.forms",
     "mezzanine.pages",
     "mezzanine.galleries",
-    "mezzanine.twitter",
+
+    #"mezzanine.twitter",
     #"mezzanine.accounts",
     #"mezzanine.mobile",
 )
@@ -334,6 +338,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.request",
     "django.core.context_processors.tz",
     "mezzanine.conf.context_processors.settings",
+    'mezzanine.pages.context_processors.page',
 )
 
 # List of middleware classes to use. Order is important; in the request phase,
