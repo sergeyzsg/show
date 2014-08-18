ddf1717095a7613a207e8bd350b2fb4428a06e1e
Sergey Chuprakov <sergeyzsg@yandex.ru>
2014-08-18 10:35:23 +0600

    initial

---
+++ fill_venv.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+. $(dirname $0)/env/bin/activate
+
+pip install -r $(dirname $0)/requirements.txt
---
+++ init_venv.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+virtualenv $(dirname $0)/env -p python --system-site-packages
